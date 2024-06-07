ebaa58bc557f1fe62a47a1e415dbe2c768009f91
veelocity <vaishnavit.006@gmail.com>
2024-06-07 17:25:02 +0530

    added admin

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Add.css";
 import { assets } from "../../assets/assets";
 import axios from "axios"
+import { toast } from "react-toastify";
 
 const Add = () => {
 
@@ -38,9 +39,10 @@ const Add = () => {
         category:"Salad"
       })
       setImage(false)
+      toast.success(response.data.message)
     }
     else {
-
+      toast.error(response.data.message)
     }
   } 
 
