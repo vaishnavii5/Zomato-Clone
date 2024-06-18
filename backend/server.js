2188ac3b899a93b1ce169ff81a8f1e318bc22a58
veelocity <vaishnavit.006@gmail.com>
2024-06-18 10:54:26 +0530

    added order placement and stripe integration

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js";
 import 'dotenv/config'
 import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
 
 // app comfig
@@ -21,8 +22,9 @@ connectDB();
 // api endpoints
 app.use("/api/food", foodRouter);
 app.use("/images", express.static('uploads'));
-app.use("/api/user", userRouter)
-app.use("/api/cart", cartRouter)
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
 
 app.get("/", (req, res) => {
